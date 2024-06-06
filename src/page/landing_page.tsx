87b5cd627820b85fa88903233ae1ff4e0fb6b303
unclebonn <khoita1008@gmail.com>
2024-06-06 08:34:25 +0700

    update animation ui

--- src/page/landing_page.tsx
+++ src/page/landing_page.tsx
@@ -1,5 +1,6 @@
-import * as React from "react";
+import { useEffect } from "react";
 import "./landingpage.css";
+import { Input } from "antd";
 
 type MyComponentProps = {};
 
@@ -7,6 +8,30 @@ const MyComponent: React.FC<MyComponentProps> = () => {
   const handleClick = (message: string) => {
     alert(message);
   };
+
+  useEffect(() => {
+    const items = document.querySelectorAll('.item');
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+          entry.target.classList.remove('hidden');
+          // observer.unobserve(entry.target);
+        }else {
+          entry.target.classList.add('hidden');
+          entry.target.classList.remove('visible');
+        }
+      },
+      {
+        rootMargin: '0px 0px -20% 0px'
+      }   
+    );
+    });
+
+    items.forEach(item => {
+      observer.observe(item);
+    });
+  }, [])
 
   return (
     <div className="div">
@@ -25,27 +50,27 @@ const MyComponent: React.FC<MyComponentProps> = () => {
         </nav>
       </header>
 
-      <section className="about">
+      <section className="about item">
         <div className="content">
           <div className="text-section">
-            <h2  className="title">VỀ CHÚNG TÔI</h2>
+            <h2 className="title">VỀ CHÚNG TÔI</h2>
             <p id="info" className="description">
-                Với thời đại phát triển và nền kinh tế khó khăn hiện tại, phụ huynh phải hết sức mình làm việc vì để cho con họ có một cuộc sống tốt hơn. Vì thế, cũng sẽ có lúc họ bận rộn mà không thể đến đón con trẻ học tại trường được, khi đó họ không thể nhờ người thân đón trẻ, thì bây giờ phải làm sao?
+              Với thời đại phát triển và nền kinh tế khó khăn hiện tại, phụ huynh phải hết sức mình làm việc vì để cho con họ có một cuộc sống tốt hơn. Vì thế, cũng sẽ có lúc họ bận rộn mà không thể đến đón con trẻ học tại trường được, khi đó họ không thể nhờ người thân đón trẻ, thì bây giờ phải làm sao?
 
-                Vì lý do đó, KidGo ra đời.  
+              Vì lý do đó, KidGo ra đời.
 
-                <br /><br />
-                Là một ứng dụng thay thế phụ huynh đưa đón con trẻ khi họ bận việc. Với tiêu chí là chất lượng và an toàn nhất có thể, phụ huynh có thể an tâm khi sử dụng dịch vụ.
+              <br /><br />
+              Là một ứng dụng thay thế phụ huynh đưa đón con trẻ khi họ bận việc. Với tiêu chí là chất lượng và an toàn nhất có thể, phụ huynh có thể an tâm khi sử dụng dịch vụ.
             </p>
           </div>
           <div className="image-section">
             <img loading="lazy" src="car.png" alt="About Us" className="img-2" />
-            
+
           </div>
         </div>
       </section>
 
-      <section className="history">
+      <section className="history item">
         <h2 className="title-center">Lịch sử hình thành</h2>
         <div className="timeline">
           <div className="timeline-item">
@@ -75,7 +100,7 @@ const MyComponent: React.FC<MyComponentProps> = () => {
         </div>
       </section>
 
-      <section className="mission">
+      <section className="mission item">
         <div className="content">
           <div className="image-section">
             <img loading="lazy" src="sumenh.png" alt="Mission" className="img-6" />
@@ -83,29 +108,29 @@ const MyComponent: React.FC<MyComponentProps> = () => {
           <div className="text-section">
             <h2 className="title">SỨ MỆNH</h2>
             <p className="emphasis">
-            Tối đa hóa an toàn cho trẻ em khi sử dụng dịch vụ đưa đón khi đến trường. Trở thành người đồng hành tin cậy hàng đầu thay phụ huynh đưa đón trẻ
-              
+              Tối đa hóa an toàn cho trẻ em khi sử dụng dịch vụ đưa đón khi đến trường. Trở thành người đồng hành tin cậy hàng đầu thay phụ huynh đưa đón trẻ
+
             </p>
           </div>
         </div>
       </section>
 
-      <section className="testimonials">
+      <section className="testimonials item">
         <h2 className="title-center">ĐÁNH GIÁ CỦA KHÁCH HÀNG</h2>
         <p className="description-center">
-            Nunc fringilla at consequat varius mauris elementum. Cras volutpat quis rhoncus rutrum sed bibendum
-            posuere. Diam sed at pulvinar sapien amet. Quam sagittis lobortis urna viverra auctor augue.
+          Nunc fringilla at consequat varius mauris elementum. Cras volutpat quis rhoncus rutrum sed bibendum
+          posuere. Diam sed at pulvinar sapien amet. Quam sagittis lobortis urna viverra auctor augue.
         </p>
         <div className="testimonials-list">
           <div className="testimonial">
             <p className="testimonial-text">
-              “Lorem ipsum dolor sit amet consectetur. Sagittis egestas nisl nisi sociis pellentesque 
+              “Lorem ipsum dolor sit amet consectetur. Sagittis egestas nisl nisi sociis pellentesque
               nibh egestas cras tempus. Vitae ullamcorper arcu lobortis lectus faucibus maecenas.
               Malesuada donec elementum nec dolor vulputate cursus velit metus ornare”
             </p>
             <div className="testimonial-info">
-            <div className="WhiteCircle">
-              <img loading="lazy" src="user.png" alt="Customer 2" className="testimonial-img" />
+              <div className="WhiteCircle">
+                <img loading="lazy" src="user.png" alt="Customer 2" className="testimonial-img" />
               </div>
               <div className="testimonial-details">
                 <h4 className="testimonial-name">Nguyễn Phương Hồng</h4>
@@ -115,13 +140,13 @@ const MyComponent: React.FC<MyComponentProps> = () => {
           </div>
           <div className="testimonial">
             <p className="testimonial-text">
-              “Lorem ipsum dolor sit amet consectetur. Sagittis egestas nisl nisi sociis pellentesque 
+              “Lorem ipsum dolor sit amet consectetur. Sagittis egestas nisl nisi sociis pellentesque
               nibh egestas cras tempus. Vitae ullamcorper arcu lobortis lectus faucibus maecenas.
               Malesuada donec elementum nec dolor vulputate cursus velit metus ornare”
             </p>
             <div className="testimonial-info">
               <div className="WhiteCircle">
-              <img loading="lazy" src="user.png" alt="Customer 2" className="testimonial-img" />
+                <img loading="lazy" src="user.png" alt="Customer 2" className="testimonial-img" />
               </div>
               <div className="testimonial-details">
                 <h4 className="testimonial-name">Trần Nguyễn Trung Quân</h4>
@@ -131,13 +156,13 @@ const MyComponent: React.FC<MyComponentProps> = () => {
           </div>
           <div className="testimonial">
             <p className="testimonial-text">
-              “Lorem ipsum dolor sit amet consectetur. Sagittis egestas nisl nisi sociis pellentesque 
+              “Lorem ipsum dolor sit amet consectetur. Sagittis egestas nisl nisi sociis pellentesque
               nibh egestas cras tempus. Vitae ullamcorper arcu lobortis lectus faucibus maecenas.
               Malesuada donec elementum nec dolor vulputate cursus velit metus ornare”
             </p>
             <div className="testimonial-info">
-            <div className="WhiteCircle">
-              <img loading="lazy" src="user.png" alt="Customer 2" className="testimonial-img" />
+              <div className="WhiteCircle">
+                <img loading="lazy" src="user.png" alt="Customer 2" className="testimonial-img" />
               </div>
               <div className="testimonial-details">
                 <h4 className="testimonial-name">Lê Đình Duy</h4>
@@ -148,20 +173,20 @@ const MyComponent: React.FC<MyComponentProps> = () => {
         </div>
       </section>
 
-      <section className="contact">
+      <section className="contact item">
         <div className="content">
           <div className="text-section">
             <div className="title-container">
               <h2 className="title">Tư vấn với KidGo ngay!</h2>
               <p className="emphasis">
-                Nunc fringilla at consequat varius mauris elementum. Cras volutpat quis rhoncus 
-                rutrum sed bibendum posuere. Diam sed at pulvinar sapien amet. Quam sagittis lobortis 
+                Nunc fringilla at consequat varius mauris elementum. Cras volutpat quis rhoncus
+                rutrum sed bibendum posuere. Diam sed at pulvinar sapien amet. Quam sagittis lobortis
                 urna viverra auctor augue.
               </p>
             </div>
           </div>
-          <div  className="image-section">
-            <img id="phoneImage"loading="lazy" src="phone.png" alt="Contact Us" className="img-10" />
+          <div className="image-section">
+            <img id="phoneImage" loading="lazy" src="phone.png" alt="Contact Us" className="img-10" />
           </div>
         </div>
       </section>
@@ -170,17 +195,17 @@ const MyComponent: React.FC<MyComponentProps> = () => {
         <div className="content">
           <div className="newsletter">
             <div className="DangKiThongBao">
-            <img loading="lazy" src="sendmail.png" alt="Newsletter" className="img-11" />
-            <h3 className="title">Đăng ký để nhận thông báo mới</h3>
+              <img loading="lazy" src="sendmail.png" alt="Newsletter" className="img-11" />
+              <h3 className="title">Đăng ký để nhận thông báo mới</h3>
             </div>
             <div className="text-section">
-              
+
               <p className="description">
                 Donec eget dignissim id sit egestas in consequat volutpat elementum donec et.
               </p>
             </div>
             <div className="input-group">
-              <input type="email" placeholder="Nhập email" className="input-email" />
+              <Input size="large" name="mail" placeholder="Nhập email" />
               <button className="btn-subscribe" onClick={() => handleClick('Đăng ký')}>Đăng ký</button>
             </div>
           </div>
